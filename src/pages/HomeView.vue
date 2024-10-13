159e2fa92496c7d5e30aef325ecf605ee5e256e7
PerfectCode01 <japhetbaz04@gmail.com>
2024-10-13 16:47:02 +0100

    debut

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,15 +1,27 @@
 <template>
-  <main class="px-32 w-screen h-screen bg-black">
+  <main class="px-72" style="background-color: #f8f7f1; height: 790px">
     <div class="flex justify-between py-8">
-      <div class="text-red-100 font-bold">JAPHET BAZ</div>
+      <div class="text-green-800 font-bold">JAPHET BAZ</div>
       <div>
-        <div class="flex gap-3 text-red-100 font-bold">
+        <div class="flex gap-3 text-green-800 font-bold">
           <div class="">Home</div>
           <div>Work</div>
           <div>About</div>
         </div>
       </div>
     </div>
+    <div class="flex justify-between pt-10">
+      <h2 class="text-7xl text-gray-800 font-bold">
+        Hey There,<br />
+        I'm Japhet
+      </h2>
+      <p class="text-xl text-gray-600">
+        I design beautifully simple things,<br />
+        and i love what i do
+      </p>
+    </div>
+    <img src="../assets/images/baz.png" alt="" class="absolute bottom-20" />
   </main>
+  <div class="w-screen h-screen text-red-600">pourquoi</div>
 </template>
 <script setup></script>
